7263eb0d970f2373e549fd758bbd203bfdb85516
taejin <taejin5314@gmail.com>
2021-05-08 16:03:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -43,12 +43,24 @@ class Layer {
   }
 }
 
-const layer4 = new Layer(backgroundLayer4, 1);
+const layer1 = new Layer(backgroundLayer1, 0.5);
+const layer2 = new Layer(backgroundLayer2, 0.5);
+const layer3 = new Layer(backgroundLayer3, 0.5);
+const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer5 = new Layer(backgroundLayer5, 1);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  layer1.update();
+  layer1.draw();
+  layer2.update();
+  layer2.draw();
+  layer3.update();
+  layer3.draw();
   layer4.update();
   layer4.draw();
+  layer5.update();
+  layer5.draw();
   requestAnimationFrame(animate);
 }
 

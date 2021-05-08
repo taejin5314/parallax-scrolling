4507a7ce134b3faa6ab563308565d90b7d2d360d
taejin <taejin5314@gmail.com>
2021-05-08 15:59:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ class Layer {
     if (this.x2 <= -this.width) {
       this.x2 = this.width + this.x - this.speed
     }
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
   }
   draw() {
 

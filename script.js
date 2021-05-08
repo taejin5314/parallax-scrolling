dd6d5314f1ddb3aebcaaad0569fc442cb801715a
taejin <taejin5314@gmail.com>
2021-05-08 15:55:59 -0400

    setting class properties

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ backgroundLayer5.src = './layer-5.png';
 
 class Layer {
   constructor(image, speedModifier) {
-
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
   }
 }
 

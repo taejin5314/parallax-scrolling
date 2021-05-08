ff562051d49ffdb3974cb34707932eee31517e4b
taejin <taejin5314@gmail.com>
2021-05-08 15:38:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   ctx.drawImage(backgroundLayer4, x, 0);
   ctx.drawImage(backgroundLayer2, x2, 0);
-  if (x < -2400) x = 2400;
+  if (x < -2400) x = 2400 - gameSpeed;
   else x -= gameSpeed;
-  if (x2 < -2400) x2 = 2400;
+  if (x2 < -2400) x2 = 2400 - gameSpeed;
   else x2 -= gameSpeed;
   requestAnimationFrame(animate);
 }

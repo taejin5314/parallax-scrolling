fa923ea4c1b6fd5721939467473ab233c79eb2ba
taejin <taejin5314@gmail.com>
2021-05-08 15:42:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = './layer-1.png';
@@ -15,17 +15,9 @@ backgroundLayer4.src = './layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = './layer-5.png';
 
-let x = 0;
-let x2 = 2400;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.drawImage(backgroundLayer4, x, 0);
-  ctx.drawImage(backgroundLayer4, x2, 0);
-  if (x < -2400) x = 2400 + x2 - gameSpeed;
-  else x -= gameSpeed;
-  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-  else x2 -= gameSpeed;
   requestAnimationFrame(animate);
 }
 
